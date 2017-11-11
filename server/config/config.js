6c6e380a54b29768fc9d1f292f6b1bdbc20a1478
phemy <phemy.smith@gmail.com>
2017-11-11 12:16:46 +0100

    added db configuration

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,10 @@ module.exports=
     "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "mrvrtguyghbyab",
+    "password": "6e4e8510384855ec667da02f08d142717ff161af4ede37b27b49e4f3a5cad14b",
+    "database": "d599a9prm8946c",
+    "host": "ec2-107-22-167-179.compute-1.amazonaws.com",
     "dialect": "postgres"
   }
 }
