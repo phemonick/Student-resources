033fffedf5bc4bfc25f8d796ea21cdf7b6758c09
phemy <phemy.smith@gmail.com>
2017-11-11 02:33:14 +0100

    added routes for finding updating and deleting resource

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 router.post('/v1/user', StudentController.createResource );
 router.get('/v1/list', StudentController.listResource);
-router.post('/v1/finduser', StudentController.getResource)
+router.post('/v1/finduser', StudentController.getResource);
+router.post('/v1/updateresource', StudentController.updateResource);
+router.post('/v1/deleteresource', StudentController.deleteResource);
 
 export default router;
